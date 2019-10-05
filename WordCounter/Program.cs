997909753db0dfda2c8687d10c7c66c2e6f81dbb
Brendon <brendonjtse@gmail.comgit config --global user.email brendonjtse@gmail.com>
2019-10-04 23:55:56 -0700

    add Wordcounter namespace to Program.cs

---
+++ WordCounter/Program.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using WordCounter.Models;
+
+namespace WordCounter
+{
+  public class Program
+  {
+    public static void Main()
+    {
+        
+    }
